014c44820aae294f14ed9a7b217fa4f0102d3508
baypathMatt <mtoomey@bppwd.com>
2016-11-16 14:07:34 -0500

    Add files via upload

---
+++ MonoSnake/Game1.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoSnake
+{
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Player pOne;
+        Gem gOne;
+        Texture2D solidText;
+        Player pOne2;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1000;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.ApplyChanges();
+        }
+        
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            solidText = new Texture2D(GraphicsDevice, 1, 1);
+            solidText.SetData(new[] { Color.White });
+
+            pOne = new Player(700, 400, solidText, Color.Red);
+            gOne = new Gem(300, 400, solidText, Color.Blue);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            pOne.Update(gameTime);
+            gOne.Update(gameTime);
+
+            if (pOne.getRect().Intersects(gOne.getRect())){
+                gOne.changeColor(Color.Black);
+                pOne2 = new Player(325, 425, solidText, Color.Red);
+                pOne2.Update(gameTime);
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            pOne.Draw(spriteBatch);
+
+            gOne.Draw(spriteBatch);
+           
+            base.Draw(gameTime);
+
+            spriteBatch.End();
+        }
+    }
+}
---
+++ MonoSnake/Gem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoSnake
+{
+    public class Gem
+    {
+
+        private Rectangle bb;
+        private Color color;
+        private Texture2D solidText;
+        private static Random rand = new Random();
+
+        public Gem(int x, int y, Texture2D texture, Color c)
+        {
+            //int xpos = rand.Next(300, 800);
+            //int ypos = rand.Next(200, 500);
+            bb = new Rectangle(x, y, 25, 25);
+            color = c;
+            solidText = texture;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+
+            sb.Draw(solidText, bb, color);
+
+        }
+
+        public Rectangle getRect()
+        {
+            return bb;
+        }
+
+        public void changeColor(Color c)
+        {
+            color = c;
+        }
+    }
+}
